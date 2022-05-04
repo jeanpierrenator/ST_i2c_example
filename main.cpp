63900751228ab6dbe5364e1cb1ea255740c34016
Guillaume Ropion <guiropion@gmail.com>
2022-05-04 12:47:18 +0000

    add digital out

--- main.cpp
+++ main.cpp
@@ -9,31 +9,33 @@
 
 // Blinking rate in milliseconds
 #define BLINKING_RATE     5000ms
-
+DigitalOut alime(PA_9);
     I2C i2c(PA_11,PA_12);
 Timer t;
 int main()
 {
+
     printf("hello\n");
     int test =0;
     
     SI1145 capteur(PA_11,PA_12);
-    t.start();
+   
+    //char test[5]; 
+    I2C_ST i2c_test(PA_11,PA_12);
+    while(1){
+        //printf(" mesure? :%d\n",sensor.IAQmeasure());
+    alime =1;  
+          t.start();
     capteur.initalize();
     test = capteur.getUV();
     test = capteur.getUV();
 
     t.stop();
+    alime =0;
     printf("The time taken was %f seconds\n", t.read());
     printf(" mesure uv :%d\n",test);
     t.reset();
-    //char test[5]; 
-    I2C_ST i2c_test(PA_11,PA_12);
-    while(1){
-        //printf(" mesure? :%d\n",sensor.IAQmeasure());
-        
-        test = capteur.getUV();
-        printf(" mesure uv :%d\n",test);
+    
         ThisThread::sleep_for(BLINKING_RATE);
     }
 }
